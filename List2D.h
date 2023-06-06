d05cee2470583078bb638d5a9cd63f0d15bd4521
Maxim <max@huzm.ru>
2023-06-06 21:31:40 +0300

    List2D improved

--- List2D.h
+++ List2D.h
@@ -14,6 +14,9 @@ public:
 	List2D();
 	// Other methods
 	bool is_empty();
+	ListElement* go_to_last();
+	void add_to_end(int val);
+
 };
 
 // Default constructor
@@ -29,5 +32,23 @@ bool List2D::is_empty()
 	else return false;
 }
 
+// Find and return last element in chain
+ListElement* List2D::go_to_last()
+{
+	if (this->is_empty()) return nullptr;
+	while (p_current->next != nullptr) {
+		p_current = p_current->next;		// Go to next
+	}
+	return p_current;
+}
 
+
+// Add new element to the end of chain
+void List2D::add_to_end(int val)
+{
+	go_to_last();
+	ListElement * created_element = new ListElement(val, p_current);
+	if (p_current != nullptr) p_current->next = created_element;
+	p_current = created_element;
+}
 
